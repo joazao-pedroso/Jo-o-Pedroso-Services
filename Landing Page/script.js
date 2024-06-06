c5e2009b3452de2d5ff132834d97ed5cbbf03ca1
joazao-pedroso <165310207+joazao-pedroso@users.noreply.github.com>
2024-06-06 15:04:09 -0300

    Landing Page

--- Landing Page/script.js
+++
@@ -1,9 +0,0 @@
-function click_hiden() {
-    var passwordField = document.getElementById('passwordInput')
-    if (passwordField.type === 'password') {
-        passwordField.type = 'text'
-    } else {
-        passwordField.type = 'password'
-    }
-}
-
